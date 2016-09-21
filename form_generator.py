009ffcc61bf99e29e1cdfc08882c6d1e503d95af
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2016-09-21 21:20:42 +0900

    first commit

---
+++ form_generator.py
@@ -0,0 +1,22 @@
+# condig: utf-8
+
+"""
+レコメンドレポートに載せる内容モジュールをインポート
+from recomodules import (SettingTemp, ReduceUsage, ChangeUsage)
+"""
+
+class FormGenerator:
+    def generate_html(self):
+        pass
+
+
+class EachHomeWayFormGemerator(FormGenerator):
+    pass
+
+
+class ClusteringWayFormGenerator(FormGenerator):
+    pass
+
+
+class ClassificationTreeWayGenerator(FormGenerator):
+    pass
