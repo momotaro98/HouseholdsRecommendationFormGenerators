912a36368414d41b8b91ddc3556b41c2556f42d2
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2016-09-25 21:57:47 +0900

    second commit

--- form_generator.py
+++ form_generator.py
@@ -5,7 +5,34 @@
 from recomodules import (SettingTemp, ReduceUsage, ChangeUsage)
 """
 
+
+class Household:
+    """
+    家庭における設計を記す
+
+    Example:
+    電力時系列データ
+    家族構成データ
+    """
+    pass
+
+
+class HouseIterator:
+    def __init__(self):
+        pass
+
+
 class FormGenerator:
+    def __init__(self, houseiter):
+        """初期化ではHouseIteratorインスタンスを受け取る
+        """
+        pass
+
+    def run(self):
+        """フォームジェネレータを実行するメソッド
+        """
+        pass
+
     def generate_html(self):
         pass
 
@@ -18,5 +45,29 @@ class ClusteringWayFormGenerator(FormGenerator):
     pass
 
 
-class ClassificationTreeWayGenerator(FormGenerator):
+class ClassificationTreeWayFormGenerator(FormGenerator):
     pass
+
+
+if __name__ == "__main__":
+    pass
+
+    # 始めにレコメンドレポートを発行する家庭群を用意する
+    # データセットからの家庭群をHouseIteratorとして収納
+    # CSVとかDBからのデータをHouseIteratorにする
+    # houses = HouseIterator("データセットからのデータ")
+
+    # instanciate EachHomeWayFormGemerator
+    # ehw_fg = EachHomeWayFormGemerator(houses)
+    # run EachHomeWayFormGemerator instance
+    # ehw_fg.run()
+
+    # instanciate ClusteringWayFormGenerator
+    # cw_fg = ClusteringWayFormGenerator(houses)
+    # run ClusteringWayFormGenerator instance
+    # cw_fg.run()
+
+    # instanciate ClassificationTreeWayFormGenerator
+    # ctw_fg = ClassificationTreeWayFormGenerator(houses)
+    # run ClassificationTreeWayFormGenerator instance
+    # ctw_fg.run()
