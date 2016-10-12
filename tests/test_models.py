22d6a6d401c1330d937d4e0c9bd692b17256bf80
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2016-10-12 16:45:10 +0900

    write models' tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,7 +1,36 @@
 from unittest import TestCase
 
 from household_recommendation_form_generators \
-    import (Household, HouseholdIterator)
+    import (Household, HouseholdGroup, HouseholdIterator,
+            RecommendModulesUseFlags, ACLogDataRows, UseFlagSwitcher,
+            FormGenerator)
+
+
+class HouseholdTestCase(TestCase):
+    def test_basic_function(self):
+        house = Household(10000)  # Instanciation
+        self.assertEqual(house.id, 10000)
+        self.assertIsInstance(
+            house.module_use_flgas, RecommendModulesUseFlags)
+
+    def test_get_data_rows_methods(self):
+        house = Household(10001)  # Instanciation
+        ac_log = house.get_ac_log()
+        self.assertIsInstance(ac_log, ACLogDataRows)
+
+
+class HouseholdGroupTestCase(TestCase):
+    def test_basic_function(self):
+        house_group = HouseholdGroup()
+        # Input Phase
+        for home_id in range(10011, 10021):
+            house = Household(home_id)
+            house_group.append(house)
+        self.assertEqual(len(house_group._households_list), 10)
+        # Output Phase
+        for house in house_group.get_iter():
+            self.assertIsInstance(house, Household)
+        self.assertEqual(len(house_group._households_list), 10)
 
 
 class HouseholdIteratorTestCase(TestCase):
@@ -44,3 +73,70 @@ class HouseholdIteratorTestCase(TestCase):
         self.assertEqual(len(house_iter._households_list), 2)
         for house in house_iter:
             self.assertIsInstance(house, Household)
+
+
+class RecommendModulesUseFlagsTestCase(TestCase):
+    def test_basic_function(self):
+        # Instanciation
+        flags = RecommendModulesUseFlags()
+        self.assertTrue(flags.use_ST)
+        self.assertTrue(flags.use_RU)
+        self.assertTrue(flags.use_CU)
+
+        # Change the flags
+        flags.use_ST = False
+        self.assertFalse(flags.use_ST)
+        flags.use_RU = False
+        self.assertFalse(flags.use_RU)
+        flags.use_CU = False
+        self.assertFalse(flags.use_CU)
+
+        # Test not change if invalid type
+        flags.use_ST = 'True'
+        self.assertFalse(flags.use_ST)
+        flags.use_RU = 'True'
+        self.assertFalse(flags.use_RU)
+        flags.use_CU = 'True'
+        self.assertFalse(flags.use_CU)
+
+    def test_reset_method(self):
+        flags = RecommendModulesUseFlags()
+        flags.use_ST = False
+        flags.use_CU = False
+        flags.reset()
+        self.assertTrue(flags.use_ST)
+        self.assertTrue(flags.use_RU)
+        self.assertTrue(flags.use_CU)
+
+
+class UseFlagSwitcherTestCase(TestCase):
+    def setUp(self):
+        self.house_group = HouseholdGroup()
+        for home_id in range(10021, 10031):
+            house = Household(home_id)
+            self.house_group.append(house)
+
+    def test_reset_method(self):
+        for house in self.house_group.get_iter():
+            house.module_use_flgas.use_ST = False
+        for house in self.house_group.get_iter():
+            self.assertFalse(house.module_use_flgas.use_ST)
+        for house in self.house_group.get_iter():
+            fs = UseFlagSwitcher(house)
+            fs.reset()
+        for house in self.house_group.get_iter():
+            self.assertTrue(house.module_use_flgas.use_ST)
+
+
+class FormGeneratorTestCase(TestCase):
+    def setUp(self):
+        self.house_group = HouseholdGroup()
+        for home_id in range(10031, 10041):
+            house = Household(home_id)
+            self.house_group.append(house)
+
+    def test_basic_function(self):
+        for house in self.house_group.get_iter():
+            duration = 'from 2016-08-01 to 2016-08-07'
+            form_generator = FormGenerator(house, duration)
+            form_generator.run()
